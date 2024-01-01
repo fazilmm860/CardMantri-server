511e90f1e646dbef230d7c6f023db2d9042e9d60
Mohamed Fazil M <fazilmm860@gmail.com>
2024-01-01 17:22:13 +0530

    added the edit and delete feature in cards

--- Router/CreditCard.Router.js
+++ Router/CreditCard.Router.js
@@ -1,11 +1,13 @@
 const express=require('express');
-const { dataPost, dataGet, specficDataGet } = require('../controllers/CreditCard');
+const { dataPost, dataGet, specficDataGet,editCreditCard,deleteCreditCard } = require('../controllers/CreditCard');
 
 const router=express.Router()
 
 router.post('/postcreditcard',dataPost);
 router.get('/getcreditcard',dataGet);
-router.get('/getSpecificreditcard/:bankName',specficDataGet)
+router.get('/getSpecificreditcard/:cardName',specficDataGet);
+router.put('/editcard/:id',editCreditCard);
+router.delete('/delete/:id',deleteCreditCard);
 
 
 module.exports=router;
--- controllers/CreditCard.js
+++ controllers/CreditCard.js
@@ -48,5 +48,48 @@
             res.status(500).json({message:error.message})
         }
     }
+    const editCreditCard=async(req,res)=>{
+        try{
+            const cardId=req.params.id;
 
-    module.exports={dataPost,dataGet,specficDataGet}
+            const updateCreditCard={
+                cardName:req.body.cardName,
+                catergory:req.body.catergory,
+                description: req.body.description,
+                img: req.body.img,
+                features: req.body.features,
+                eligibility: req.body.eligibility,
+                document: req.body.document,
+                charge: req.body.charge,
+                faq: req.body.faq,
+                faqAns: req.body.faqAns
+            }
+
+            // Find the card by ID and Update the data
+            const card=await CreditCard.findByIdAndUpdate(cardId,updateCreditCard,{new:true});
+
+            if(!card){
+                return res.status(404).json({message:'Card Not Found'})
+            }
+            res.status(200).json({message:"Card Data updated successfully"})
+        }catch (error){
+            res.status(400).json({message:error.message});
+        }
+    };
+    const deleteCreditCard=async(req,res)=>{
+        try {
+            const cardId=req.params.id;
+
+            // find the card by Id and Delete it
+
+            const deletedCard=await CreditCard.findByIdAndDelete(cardId);
+
+            if(!deletedCard){
+                return res.status(404).json({message:'Card Not Found'});
+            }
+            res.status(200).json({message:'Card Data deleted Successfully',data:deletedCard});
+        } catch (error) {
+            res.status(400).json({message:error.message})
+        }
+    }
+    module.exports={dataPost,dataGet,specficDataGet,editCreditCard,deleteCreditCard}
